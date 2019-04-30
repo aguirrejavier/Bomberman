b45616f470e2c4d000c92a7583a499f73716a90d
aguirrecarlos <aguirrecarlos1995@gmail.com>
2019-04-30 16:11:48 -0300

    bomberman

--- src/Escenario/Escenario.java
+++ src/Escenario/Escenario.java
@@ -2,45 +2,48 @@ package Escenario;
 
 import java.util.Random;
 
-public class Escenario {
+public  class Escenario {
 	
-	private static int TAM;
-	private static int mat[][];
-	private static int cantJugadores;
+	protected static final int BLOQUE_FIJO=0;
+	protected static final int BLOQUE_DESTRUIBLE=1;
+	protected static final int ESPACIO_EN_BLANCO=2;
 	
-	public Escenario(int tam, int cantJugadores) {	//EL TAMAÑO DEL ESCENARIO DEBE SER IMPAR Y >= 3
+	private static final int TAM=9;
+	public int mat[][];
+	private int cantJugadores=0;
+	
+	public Escenario() {	//EL TAMAÑO DEL ESCENARIO DEBE SER IMPAR Y >= 3
 		
-		this.TAM=tam;
-		this.mat = new int[tam][tam];
-		this.cantJugadores=cantJugadores;
+		this.mat = new int[TAM][TAM];
 		
 		Random aleatorio = new Random(System.currentTimeMillis());
 		
 		// 0 => BLOQUE FIJO, 1 => BLOQUE DESTRUIBLE , 2 => ESPACIO EN BLANCO,  3 O MAS JUGADOR
 		
-		for(int i=0; i<tam; i++) {
-			for(int j=0; j<tam; j++) {
-				if(i==0 || i==tam-1 || j==0 || j==tam-1 || (i%2==0 && j%2==0)) { //SI ES UN BORDE O UNA DIAGONAL PAR => BLOQUE FIJO
+		for(int i=0; i<TAM; i++) {
+			for(int j=0; j<TAM; j++) {
+				if(i==0 || i==TAM-1 || j==0 || j==TAM-1 || (i%2==0 && j%2==0)) { //SI ES UN BORDE O UNA DIAGONAL PAR => BLOQUE FIJO
 					mat[i][j]=0;
 				}else {
 					mat[i][j]=aleatorio.nextInt(2)+1; //EN DONDE NO HAY FIJOS PONGO ALEATORIAMENTE (BLOQUE FIJO / ESPACIO EN BLANCO) IGUALMENTE VOY A PISAR LOS BLOQUES QUE ESTAN CERCA DE LOS JUGADORES
 				}
 			}
 		}
-		
-		if(cantJugadores>=1) {
-			mat[1][1]=3; mat[1][2]=2; mat[2][1]=2; // PISO CON ESPACIOS (2) Y PONGO EL PRIMER JUGADOR(3)
-			if(cantJugadores>=2) {
-				mat[tam-2][tam-2]=4; mat[tam-2][tam-3]=2; mat[tam-3][tam-2]=2; // PISO CON ESPACIOS (2) Y PONGO EL SEGUNDO JUGADOR(4)
-				if(cantJugadores>=3) {
-					mat[1][tam-2]=5; mat[1][tam-3]=2; mat[2][tam-2]=2; // PISO CON ESPACIOS (2) Y PONGO EL TERCER JUGADOR(5)
-					if(cantJugadores>3) {
-						mat[tam-2][1]=6; mat[tam-2][2]=2; mat[tam-3][1]=2; // PISO CON ESPACIOS (2) Y PONGO EL CUARTO JUGADOR(6)
-					}
-				}
-			}
+	
+	}
+	public boolean crearJugador(Bomberman bomber) {
+		if(this.cantJugadores>=4)
+			return false;
+		this.cantJugadores++;
+		if(this.cantJugadores == 1) {
+			bomber.posicionX = 1;
+			bomber.posicionY = 1;
+			bomber.numJugador = 3;
+			this.mat[bomber.posicionX][bomber.posicionY] = bomber.numJugador;
+			this.mat[1][2] = ESPACIO_EN_BLANCO;
+			this.mat[2][1] = ESPACIO_EN_BLANCO;
 		}
-			
+		return true;
 	}
 	
 	public void dibujar() {	//PARA VER EN LA CONSOLA
@@ -66,7 +69,12 @@ public class Escenario {
 	
 	
 	public static void main(String[] args) {
-		Escenario escenario = new Escenario(9, 4);
+		Escenario escenario = new Escenario();
+		Bomberman bomber = new Bomberman();
+		escenario.crearJugador(bomber);
+		//bomber.moverHaciaAbajo(escenario);
+		//System.out.println(bomber.posicionX+ " "+ bomber.posicionY);
+		
 		escenario.dibujar();
 	}
 	
